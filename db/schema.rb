fc789e3461783a203339377106745a00df860627
luccafort <muspellsheim1985@gmail.com>
2018-05-19 00:46:04 +0900

    [modify]add to new models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516160744) do
+ActiveRecord::Schema.define(version: 20180518153259) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
-    t.text "uuid"
+    t.text "uuid", null: false, comment: "accountsとusersを紐付けるユニークなハッシュ値"
+    t.string "email", comment: "ログインやアカウント登録に必要なemail"
+    t.string "name", comment: "アカウント名(著者名とは違う)"
+    t.string "password", null: false, comment: "暗号化されたパスワード"
+    t.string "icon_iamge", comment: "アイコン画像"
+    t.datetime "arrival_at", comment: "アカウント承認の日時"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer "account_id", comment: "アカウントID(著者とアカウントは別扱い)"
+    t.string "handle_name", comment: "ハンドルネーム(ペンネーム、著者)"
+    t.string "icon_image", comment: "登録されているアイコン画像"
+    t.text "profile", comment: "自己紹介文などのプロフィール"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookmarks", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer "account_id", comment: "ユーザID"
+    t.integer "book_id", comment: "小説の作品ID"
+    t.integer "story_id", comment: "栞のあるストーリーID"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "title", comment: "小説の作品タイトル"
+    t.integer "status", comment: "小説の状態[\"連載中\",\"連載休止\",\"完結済み\"]"
+    t.integer "summary_id", comment: "あらすじの参照ID", unsigned: true
+    t.integer "description_id", comment: "小説の紹介情報ID", unsigned: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stories", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.text "story", comment: "小説本文"
+    t.integer "status", comment: "ストーリーの状態[\"ドラフト(下書き)\", \"掲載\", \"掲載中止\"]"
+    t.integer "book_id", comment: "小説の作品ID"
+    t.datetime "published_at", comment: "公開日時(公開予約日時)"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
