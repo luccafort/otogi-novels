50a439de01aa8b35865ab013e01f940a2881b29c
luccafort <muspellsheim1985@gmail.com>
2018-05-19 00:38:12 +0900

    [modify]merged users to account.アカウント情報とユーザ情報を分割管理する予定だったがそこまで杞憂な実装なので廃止、アカウントで全て賄うよう変更

--- db/migrate/20180516160744_create_accounts.rb
+++ db/migrate/20180516160744_create_accounts.rb
@@ -1,7 +1,12 @@
 class CreateAccounts < ActiveRecord::Migration[5.1]
   def change
     create_table :accounts do |t|
-      t.text :uuid
+      t.text :uuid, :unique => true, null: false, comment: 'accountsとusersを紐付けるユニークなハッシュ値'
+      t.string :email, comment: 'ログインやアカウント登録に必要なemail'
+      t.string :name, comment: 'アカウント名(著者名とは違う)'
+      t.string :password, :unique => true, null: false, comment: '暗号化されたパスワード'
+      t.string :icon_iamge, comment: 'アイコン画像'
+      t.datetime :arrival_at, comment: 'アカウント承認の日時'
 
       t.timestamps
     end
