6556a1ee28400840c7a3a50aa090b7989d9c8fb5
luccafort <muspellsheim1985@gmail.com>
2018-05-22 11:57:15 +0900

    [WIP]TODOコメント追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # TODO:著者の所有者アカウントのみアクセス可能(小説の編集、削除、登録などなど)
+  # TODO:アカウント認証チェック
+  # TODO:レスポンスの共通化(正常系)
+  # TODO:レスポンスの共通化(異常系)
 end
