6a51f4245be85643c8deadc2fd54d4d328216f4a
luccafort <muspellsheim1985@gmail.com>
2019-05-11 15:21:05 +0900

    modify to correct word.

--- db/migrate/20180518145318_create_stories.rb
+++ db/migrate/20180518145318_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration[5.1]
   def change
     create_table :stories, id: :bigint, unsigned: true do |t|
       t.integer :book_id, limit: 8, unsigned: true, comment: '小説の作品ID'
-      t.text :story, comment: '小説本文'
+      t.text :content, comment: '小説本文'
       t.integer :status, limit: 1, unsigned: true, comment: 'ストーリーの状態["下書き(draft)", "掲載(publish)", "掲載中止(paused)", "掲載停止(banned)"]'
       t.datetime :published_at, comment: '公開日時(公開予約日時)'
 
--- db/migrate/20180518145604_create_bookmarks.rb
+++ db/migrate/20180518145604_create_bookmarks.rb
@@ -2,10 +2,9 @@ class CreateBookmarks < ActiveRecord::Migration[5.1]
   def change
     create_table :bookmarks, id: false, primary_key: [:account_id, :book_id, :story_id] do |t|
       t.integer :account_id, limit: 8, unsigned: true, comment: 'ユーザID'
-      t.string :booknark_name, comment: 'ブックマーク名'
-      t.boolean :status, comment: 'Enum[:public(公開)、:private(非公開))'
-      t.integer :book_id, comment: '小説の作品ID'
-      t.integer :story_id, comment: '栞のあるストーリーID'
+      t.string :label, null: false, comment: 'ラベル(ブックマーク名)'
+      t.boolean :published, default: true, null: false, comment: '公開|非公開'
+      t.integer :story_id, null: false, comment: '栞のあるストーリーID'
 
       t.timestamps
     end
